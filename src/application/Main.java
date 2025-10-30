ad50c09158c3b683a68e47dcc27eed4c0466b69a
LuisFenandoV14 <venturaluisf7@outlook.com>
2025-10-30 16:49:31 -0300

    Creating Department class
    Created Department as a class.
    Also changed Main so I could test if Department is working as intended.
    Also created packages so the project is better structured.

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department department = new Department("Electronics", 1 );
+        System.out.println(department);
+
+    }
+}
---
+++ src/model/entities/Department.java
@@ -0,0 +1,66 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // Attributes
+    private Integer id;
+    private String name;
+
+    // Constructors
+    public Department(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+    public Department() {}
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // hashCode and equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+        return getId().equals(that.getId()) && getName().equals(that.getName());
+        /* ---
+        Two departments are equal if their Id and Name are the same
+        Dois departamentos são iguais se o Id e o Nome forem o mesmo
+        --- */
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + getName().hashCode();
+        return result;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Department: {" +
+                "name = " + name +
+                ", id = " + id + "}";
+        // Example: "Department: {name = Electronics, id = 1}"
+    }
+
+}
